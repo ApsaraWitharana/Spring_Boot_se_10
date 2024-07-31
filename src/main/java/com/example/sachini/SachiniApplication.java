7477d55826e0f952d0b6d5df8f53c2e948500b68
ApsaraWitharana <asachini095@gmail.com>
2024-07-31 12:16:04 +0530

    update-spring boot project

---
+++ src/main/java/com/example/sachini/SachiniApplication.java
@@ -0,0 +1,13 @@
+package com.example.sachini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SachiniApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SachiniApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/sachini/controller/BlogController.java
@@ -0,0 +1,30 @@
+package com.example.sachini.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+//
+@RestController
+//mapping  req clss run
+//    http://localhost:8080/blog
+@RequestMapping("blog")
+public class BlogController {
+    //    http://localhost:8080/blog/hellow
+    @GetMapping("/hellow")
+    public String hellow(){
+        return "HELLO WORLD!!";
+    }
+
+    @PostMapping("/number")
+//    http://localhost:8080/blog/number =>colling postman =req type post
+    public int getNumber(){
+        return 9;
+    }
+
+    //Spring boot path variable request
+//    http://localhost:8080/blog/api/employees/C001
+    @GetMapping("/api/employees/{id}")
+    @ResponseBody
+    public String getEmployeesById(@PathVariable String id) {
+        return "ID: " + id;
+    }
+}
